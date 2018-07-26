44e415634b4bb04f496e544cae83dc051479d85b
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-07-26 05:26:39 -0700

    first

---
+++ HMM.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from hmmlearn import hmm
+
+def HMM():
+
+    #hmm_model = hmm.GaussianHMM(n_components=3, covariance_type="full")
+
+
+    
+    
+
+def main():
+
+    HMM()
+
+if __name__ == "__main__":
+  
+    main() 
